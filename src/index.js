a6884149a2ee417084b35182cb38116066c5812d
Priyam Bhattacharya <priyambhattacharya6@gmail.com>
2022-02-27 11:09:05 +0530

    completed the banner section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ console.log("Test")
 const ham = document.getElementById('ham-menu-div');
 const nav = document.getElementById('nav-menu');
 ham.addEventListener('click' , function(e){
-    nav.classList.toggle('-left-[100%]')
+    nav.classList.toggle('-left-[100vw]')
     nav.classList.toggle('left-0')
     nav.classList.toggle('transition-all')
 })
