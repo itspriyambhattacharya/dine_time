03d9853b96f452b2c61229057116bde795971726
Priyam <priyambhattacharya6@gmail.com>
2022-02-28 20:25:26 +0530

    hide navbar when click on navlinks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 console.log("Test")
 const ham = document.getElementById('ham-menu-div');
 const nav = document.getElementById('nav-menu');
-ham.addEventListener('click' , function(e){
+
+//Responsive Menu
+
+ham.addEventListener('click', function (e) {
     nav.classList.toggle('-left-[100vw]')
     nav.classList.toggle('left-0')
     nav.classList.toggle('transition-all')
 })
+
+//NAv Menu Links
+
+const nav_li = document.querySelectorAll('.nav-li>a');
+console.log(nav_li)
+
+for (let index = 0; index < nav_li.length; index++) {
+    const element = nav_li[index];
+    element.addEventListener('click', function (e) {
+        nav.classList.toggle('-left-[100vw]')
+        nav.classList.toggle('left-0')
+        nav.classList.toggle('transition-all')
+    })
+}
